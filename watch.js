6d3ec05fdf72928863346b30473c617648609f4b
Ray Deck <ray@raydeck.com>
2018-02-06 09:51:56 -0500

    Shrinking watch to just call the main executable with the watch flag

--- watch.js
+++ watch.js
@@ -1,19 +1,10 @@
 #!/usr/bin/env node
-const watch = require("node-watch");
-const core = require("./index.js");
-const commander = require("commander");
+const cp = require("child_process");
 const Path = require("path");
-commander.usage("[options] [targetpath]");
-commander.option("-o --out <outfile>");
-commander.parse(process.argv);
-var thisPath = commander.args[0];
-if (!thisPath) thisPath = process.cwd();
-thisPath = core.getRootIOSPath(thisPath);
-var outfile = commander.out;
-if (!outfile) outfile = Path.join(thisPath, "rn-swift-bridge.m");
-console.log("Watching for swift changes on " + thisPath);
-watch(thisPath, { recursive: true, filter: /\.swift$/ }, () => {
-  const text = core.getBridgingModuleTextFromPath(thisPath);
-  console.log("Detected change");
-  if (core.writeIf(outfile, text)) console.log("Updated " + outfile);
-});
+const fs = require("fs");
+
+const thisPath = fs.realpathSync(process.argv[1]);
+const clipath = Path.join(Path.dirname(thisPath), "cli.js");
+const args = [...process.argv.slice(2, process.argv.length), "--watch"];
+console.log("Running " + clipath + " with args", args);
+cp.spawnSync(clipath, args, { stdio: "inherit" });
